d822dd5c5b97b2737b1944c9ce94b37800c61faf
MHDante <mhdcj@hotmail.com>
2014-04-11 10:36:55 -0400

    Jahoo

--- iteration3wpf/Windows/CreateGroupWindow.xaml.cs
+++ iteration3wpf/Windows/CreateGroupWindow.xaml.cs
@@ -22,8 +22,20 @@ namespace iteration3wpf.Windows
         Project p;
         public CreateGroupWindow(Project p)
         {
-            this.p = p;
             InitializeComponent();
+            this.p = p;
+            foreach (var s in p.PrjCourse.Students)
+            {
+                foreach (var g in s.Groups)
+                {
+                    if (g.GrpProject == p) goto Finish;
+                }
+                listUnadded.Items.Add(s);
+
+            Finish:
+                continue;
+            }
+            
             this.CenterWindow();
         }
 
@@ -31,10 +43,47 @@ namespace iteration3wpf.Windows
         {
             Close();
         }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (listUnadded.SelectedIndex >= 0)
+            {
+                User s = (User)listUnadded.SelectedItem;
+                listAdded.Items.Add(s);
+                listUnadded.Items.Remove(s);
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (listAdded.SelectedIndex >= 0)
+            {
+                User s = (User)listUnadded.SelectedItem;
+                listUnadded.Items.Add(s);
+                listAdded.Items.Remove(s);
+            }
+        }
 
         private void Window_Closed(object sender, EventArgs e)
         {
             MainWindow.mainWindow.IsEnabled = true;
         }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtGroupName.Text))
+            {
+                MessageBox.Show("Plsease name gourp");
+                return;
+            }
+
+            Group g = Group.getNew();
+            foreach( User u in listAdded.Items)
+            {
+                g.Members.Add(u);
+                u.Groups.Add(g);
+            }
+            Close();
+        }
+
     }
 }
